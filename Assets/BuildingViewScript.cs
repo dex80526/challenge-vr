bd5007f16b27598cc81222e8a6dd974327134ac0
Dex Chen <Dexing.Chen@Colorado.EDU>
2017-04-14 15:13:21 -0600

    oda-hackathon-vr

---
+++ Assets/BuildingViewScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingViewScript : MonoBehaviour {
+
+	public Canvas canvas;
+
+	public string buildingName;
+
+	// Use this for initialization
+	void Start () {
+		canvas = this.GetComponentInChildren<Canvas> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void displayData(bool state) {
+		canvas.enabled = state;
+		//display the info about this object (builing name, year built)
+	}
+
+	public string getBuildingData() {
+		return buildingName;
+	}
+		
+}
---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+	public bool collision;
+	public GameObject body;
+	public Canvas personDisplay;
+	public Text textDisplay; 
+
+
+	// Use this for initialization
+	void Start () {
+		personDisplay = this.gameObject.GetComponentsInChildren<Canvas> ()[0]; 
+		textDisplay = this.gameObject.GetComponentInChildren<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//detect if the person is in the Building
+		//if yes, display some info about the building: name of building, year built, functioning/services, etc
+		//let's first hard code the data now. the data could be looked up from other service or db based on the building id.
+		//if not, do nothing
+	}
+		
+
+	void OnTriggerEnter(Collider other) {
+		Component otherComponent;
+		if (other.tag == "building") {
+			collision = true;
+
+			otherComponent = other.gameObject.GetComponent<BuildingViewScript> (); //.displayData (collision);
+			string buildingData = ((BuildingViewScript)otherComponent).getBuildingData();
+			Debug.Log (buildingData);
+			showBuildingData(buildingData, true);
+
+		}
+
+		if (other.tag == "parkinglot") {
+			//myComponet = other.gameObject.GetComponent<BuildingViewScript> (); //.displayData (collision);
+			//showParkingData();
+		}
+
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "building") {
+			collision = false;
+			showBuildingData("", false);
+		}
+	}
+
+	void showBuildingData(string data, bool show) {
+		textDisplay.text = data;
+		personDisplay.enabled = show;
+	}
+
+}
